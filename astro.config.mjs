dbbb5fca7b9d69a6c37ec33f735ae4f48f3466d3
jaspery <jasper.yzh@gmail.com>
2024-11-25 15:28:29 +0800

    update deploy.yml for deno2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
+// import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  
+  site: "https://jasperyong.com",
+  // base: '/',
+
+  integrations: [
+    // mdx(), 
+    sitemap()
+  ],
+
+  
 });
