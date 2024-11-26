93ed69f5ec238b0552ed7b5bc37da9d4aba13a81
jaspery <jasper.yzh@gmail.com>
2024-11-26 20:40:12 +0800

    establish the layout and fonts; added carousel

--- src/consts.ts
+++ src/consts.ts
@@ -5,9 +5,13 @@
 // export const SITE_DESCRIPTION = 'Welcome to my website!';
 
 export const SITE_YEAR = new Date().getFullYear();
-export const SITE_TITLE = "JasperYong";
+export const SITE_TITLE = "jy.com";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const SITE_FOOTER_ABOUT =
   "<b>Thank you for visiting my website!</b><br>This blog are collections of my learnings, experiments, and personal projects. I hope you find something that inspires or resonates with you. Enjoy exploring, and come back soon!";
 export const RESUME_LINK = "";
 // export const RESUME_LINK = "/241009-resume.pdf"
+
+
+export const LANDING_TITLE = "Learning\\\\NonStop";
+export const LANDING_DESC = "A portfolio site of Jasper's work. Based in KL, Malaysia. Currently Freelancing around town.";
